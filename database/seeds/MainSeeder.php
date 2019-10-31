e7e6259c15e0b83c10ae0b9601069f159cc2fb0b
Obe <jansen.obe@gmail.com>
2019-10-31 22:48:53 +0100

    Appointments implemented

---
+++ database/seeds/MainSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
